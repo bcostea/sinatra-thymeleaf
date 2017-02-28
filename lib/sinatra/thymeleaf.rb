cb0fb5b04ed4af42e345d5809fc9c2701c18284e
Bogdan Costea <bogdan@costea.us>
2017-02-28 22:39:18 +0000

    Added support for warden current_user, data-th-insert and data-th-replace settings, new gem version

--- lib/sinatra/thymeleaf.rb
+++ lib/sinatra/thymeleaf.rb
@@ -3,16 +3,26 @@ require_relative 'tilt/thymeleaf'
 require 'i18n'
 
 module Sinatra
+  # we need to configure the Thymeleaf prefix and suffix for insert and replace
+  Thymeleaf.configuration.template.prefix = "./views/"
+  Thymeleaf.configuration.template.suffix = ".html"
 
   module Thymeleaf
     def thyme(template=nil, locals={}, options = {}, &block)
+
       options, template = template, nil if template.is_a?(Hash)
       template = lambda { block } if template.nil?
       locals[:t] = lambda { |m| I18n.t(m) }
 
+      # setup Sinatra flash
       locals[:flash] = {}
       unless flash.nil?
         locals[:flash] = flash
+      end
+
+      # add warden current_user if available
+      unless current_user.nil?
+        locals[:current_user] = current_user
       end
 
       render :html, template, options, locals
